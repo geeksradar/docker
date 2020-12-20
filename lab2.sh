a586d09396095c4116c4ad97e3edc625ada0a400
Prateek Jain <prateekjain0495@gmail.com>
2020-12-20 18:19:50 +0530

    Update lab2.sh

--- lab2.sh
+++ lab2.sh
@@ -3,6 +3,9 @@
 # Initializing Configuration
 #gcloud init < a
 
+ID=$(gcloud info --format='value(config.project)')
+gcloud config set project $ID
+
 export IMAGE_FAMILY="windows-2012-r2-core"
 export ZONE="us-central1-a"
 export INSTANCE_NAME="my-new-instance"
